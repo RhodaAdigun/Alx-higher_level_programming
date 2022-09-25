a599e9849bfa86fec71649ed4e531c7ec1cd8448
RhodaAdigun <ifeoluwa.adigun2102@gmail.com>
2022-09-25 12:15:42 +0000

    prints arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 1
+    arg = len(sys.argv)
+    if arg == 1:
+        print("0 arguments.")
+    elif arg == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(arg - 1))
+        while i < arg:
+            print("{:d}: {}".format(i, sys.argv[i]))
+            i += 1
