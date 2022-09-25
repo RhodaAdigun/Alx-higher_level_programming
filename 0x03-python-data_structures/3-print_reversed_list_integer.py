406a2a4a84375bee5d1bf6332a29c75367d0285e
RhodaAdigun <ifeoluwa.adigun2102@gmail.com>
2022-09-25 18:48:08 +0000

    lists

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    count = len(my_list)
+
+    while count != 0:
+        print("{:d}".format(my_list[count - 1]))
+        count -= 1
